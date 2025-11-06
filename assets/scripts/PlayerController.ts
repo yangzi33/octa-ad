b1033ceb498ad6bcd158acf3bf5ccad6543e7354
Ivan Yang <ivan.yang@igg.com>
2025-11-06 06:18:21 -0800

    no message

--- assets/scripts/PlayerController.ts
+++ assets/scripts/PlayerController.ts
@@ -444,6 +444,7 @@ export class PlayerController extends Component {
         }
     }
     
+    
     stabilizePlayer() {
         if (this._rigidBody) {
             const currentPos = this.node.position;
